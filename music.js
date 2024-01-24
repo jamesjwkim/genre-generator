25a5d137dc073441ce457018d201e80c82ba1939
Jinwoo Kim <jinwookim9911@gmail.com>
2024-01-24 14:15:01 -0800

    no consecutive index

--- music.js
+++ music.js
@@ -18,22 +18,33 @@ const generateButton = document.getElementById("generateButton");
 resultSpan.textContent = "no genre selected..."
 resultSpan.style.color = "grey"
 
+// initalize with a value that won't occur in the selected array
+let prevIndex = -1
 
 function getRandomItem() {
-    const randomIndex = Math.floor(Math.random() * genres.length);
+
+  let randomIndex;
+
+  do {
+    randomIndex = Math.floor(Math.random() * genres.length);
+  } while (randomIndex === prevIndex)
+
     console.log("button pressed")
-   // Set initial opacity to 0
+   
     resultSpan.textContent = genres[randomIndex]
     resultSpan.style.color = "black" 
 
     generateButton.style.border = "2px solid white"; // Change border color
 
     // Remove and re-add the element to trigger the animation again
-    resultSpan.style.animation = "none";
-    void resultSpan.offsetWidth; // Trigger reflow
+
+    // resultSpan.style.animation = "none";
+    // void resultSpan.offsetWidth; // Trigger reflow
+
     // Add a fade-in animation
     resultSpan.style.animation = "fadeIn 0.5s ease-in-out";
 
+    prevIndex = randomIndex;
 
     setTimeout(() => {
       generateButton.style.border = "2px solid #000"; // Reset border color after a short delay
